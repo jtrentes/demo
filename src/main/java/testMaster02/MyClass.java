cb46f2851e4b92609bad27ed2c795f88c4dc962d
genmymodel <help@genmymodel.com>
2013-05-27 17:25:13 +0200

    commit by genmymodel from www.genmymodel.com: v20130527_172511

---
+++ src/main/java/testMaster02/MyClass.java
@@ -0,0 +1,31 @@
+package testMaster02;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class MyClass
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public MyClass2 myclass2;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public MyClass(){
+		
+	}
+
+}
+
---
+++ src/main/java/testMaster02/MyClass2.java
@@ -0,0 +1,31 @@
+package testMaster02;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class MyClass2
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public MyClass myclass;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public MyClass2(){
+		
+	}
+
+}
+
