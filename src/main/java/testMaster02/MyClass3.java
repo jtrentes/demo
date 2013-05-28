647deb7dd7a13c1cea38e361d6857749ac76afdf
genmymodel <help@genmymodel.com>
2013-05-28 09:48:29 +0200

    commit by genmymodel from www.genmymodel.com: v20130528_094827

--- src/main/java/testMaster02/MyClass3.java
+++ src/main/java/testMaster02/MyClass3.java
@@ -16,6 +16,38 @@ public class MyClass3
 	 * @ordered
 	 */
 	
+	public String attribute;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String attribute2;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String attribute3;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String attribute4;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
 	public MyClass2 myclass22;
 
 	/**
@@ -25,6 +57,39 @@ public class MyClass3
 	 */
 	public MyClass3(){
 		
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void operation() {
+		// TODO : to implement	
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void operation2() {
+		// TODO : to implement	
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void operation3() {
+		// TODO : to implement	
 	}
 
 }
